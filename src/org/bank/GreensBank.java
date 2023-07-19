1cee3de0adb5a25ebf61c29a7744158e9f7b6ace
Aarthi-sys <aarthidakshnamoorthi@gmail.com>
2023-07-19 17:42:31 +0530

    First time push

---
+++ src/org/bank/GreensBank.java
@@ -0,0 +1,18 @@
+package org.bank;
+
+public class GreensBank {
+
+	private void bank1() {
+
+		System.out.println("TamilNadu");
+	}
+	
+	private void bank2() {
+
+		System.out.println("Kerala");
+	}
+	private void bank3() {
+
+		System.out.println("AndhraPradesh");
+	}
+}
---
+++ src/org/bank/ICICIBank.java
@@ -0,0 +1,13 @@
+package org.bank;
+
+public class ICICIBank {
+	
+	private void bank1() {
+		System.out.println("TamilNadu");
+	}
+	
+	private void bank2() {
+		System.out.println("Bangalore");
+	}
+	
+}
